28478cc2154608d2d8f05e3d97de62b5417137e8
Hoxton <hoxton019030@gmail.com>
2023-07-07 01:20:31 +0800

    reset

--- src/main/java/com/example/householdregistrationsystem/repository/FatherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.householdregistrationsystem.repository;
-
-import com.example.householdregistrationsystem.entity.Father;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FatherRepository extends JpaRepository<Father,Long> {
-}
